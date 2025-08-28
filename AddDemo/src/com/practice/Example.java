432ec7f155ed41c8981231f19f86fc89cdcd1a75
unknown <kamleshkarwal4@gmail.com>
2025-08-28 18:15:27 +0530

    Change the code

--- AddDemo/src/com/practice/Example.java
+++ AddDemo/src/com/practice/Example.java
@@ -4,6 +4,8 @@ public class Example {
 
 	public static void main(String[] args) {
 		
-		System.out.println("kjfkjnkscc ;kjcoijkjc ");
+		System.out.println("kjfkjnkscc ;kjcoijkjc lkklm,m ,m m ");
+		
+		System.out.println("Hello Kamlesh");
 	}
 }
